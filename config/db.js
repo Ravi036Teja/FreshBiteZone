2aa0011b17c0cd22cbe41e92b12174958fe7152a
Ravi <rteja6067@gmail.com>
2025-06-01 14:16:22 +0530

    Resolved merge conflicts in db.js

--- config/db.js
+++ config/db.js
@@ -16,32 +16,67 @@
 // module.exports = connectDB;
 
 // config/db.js
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+// <<<<<<< HEAD
+//       useUnifiedTopology: true,
+//       serverSelectionTimeoutMS: 5000,
+//       socketTimeoutMS: 45000,
+//       family: 4
+//     });
+//     console.log('MongoDB Connected...');
+// =======
+//       useUnifiedTopology: true
+//     });
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+// >>>>>>> 4334d9c3bc17205670c6354b60184aa4a02a6708
+//   } catch (err) {
+//     console.error('Initial connection error:', err.message);
+//     process.exit(1);
+//   }
+// };
+
+// <<<<<<< HEAD
+// // Add these listeners outside the connectDB function
+// mongoose.connection.on('error', err => {
+//   console.error(`DB connection error: ${err.message}`);
+// });
+
+// mongoose.connection.on('disconnected', () => {
+//   console.log('DB disconnected');
+// });
+
+// mongoose.connection.on('reconnected', () => {
+//   console.log('DB reconnected');
+// });
+
+// =======
+// >>>>>>> 4334d9c3bc17205670c6354b60184aa4a02a6708
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-<<<<<<< HEAD
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
       family: 4
     });
-    console.log('MongoDB Connected...');
-=======
-      useUnifiedTopology: true
-    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
->>>>>>> 4334d9c3bc17205670c6354b60184aa4a02a6708
   } catch (err) {
     console.error('Initial connection error:', err.message);
     process.exit(1);
   }
 };
 
-<<<<<<< HEAD
-// Add these listeners outside the connectDB function
+// Connection event listeners
 mongoose.connection.on('error', err => {
   console.error(`DB connection error: ${err.message}`);
 });
@@ -54,6 +89,4 @@ mongoose.connection.on('reconnected', () => {
   console.log('DB reconnected');
 });
 
-=======
->>>>>>> 4334d9c3bc17205670c6354b60184aa4a02a6708
 module.exports = connectDB;
